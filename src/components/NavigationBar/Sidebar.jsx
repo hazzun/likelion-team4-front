86a71cee120aaff83b9fd04a9f21750c37e2357e
hajoon <gkwns0516@gmail.com>
2023-07-07 14:08:48 +0900

    [Design] 사이드바 UI구현

--- src/components/NavigationBar/Sidebar.jsx
+++ src/components/NavigationBar/Sidebar.jsx
@@ -1,14 +1,62 @@
 import React from "react";
 import "./Sidebar.css";
+import { AiFillHome } from "react-icons/ai";
+import { BsDownload } from "react-icons/bs";
+import { BiMusic, BiMovie } from "react-icons/bi";
+import { GoVideo } from "react-icons/go";
+import { GrStorage } from "react-icons/gr";
+import { TbClockRecord } from "react-icons/tb";
+import {
+  MdOutlineAppShortcut,
+  MdOutlineUnsubscribe,
+  MdContentCopy,
+} from "react-icons/md";
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <aside id="sidebar">
-      sidebar
-      {/* <section>홈</section>
-      <section>Shorts</section>
-      <section>구독</section>
-      <section>YouTube Music</section> */}
+      <button className="sideBtn">
+        <AiFillHome size={24} />
+        <span>홈</span>
+      </button>
+      <button className="sideBtn">
+        <MdOutlineAppShortcut size={24} />
+        <span>Shorts</span>
+      </button>
+      <button className="sideBtn">
+        <MdOutlineUnsubscribe size={24} />
+        <span>구독</span>
+      </button>
+      <button className="sideBtn">
+        <BiMusic size={24} />
+        <span>TouTube Music</span>
+      </button>
+      <button className="sideBtn">
+        <MdContentCopy size={24} />
+        <span>보관함</span>
+      </button>
+      <button className="sideBtn">
+        <TbClockRecord size={24} />
+        <span>시청 기록</span>
+      </button>
+      <button className="sideBtn">
+        <GoVideo size={24} />
+        <span>내 동영상</span>
+      </button>
+      <button className="sideBtn">
+        <BiMovie size={24} />
+        <span>내 영화</span>
+      </button>
+      <button className="sideBtn">
+        <GrStorage size={24} />
+        <span>나중에 볼 동영상</span>
+      </button>
+      <button className="sideBtn">
+        <BsDownload size={24} />
+        <span>오프라인 저장 동영상</span>
+      </button>
+      <button className="sideBtn"></button>
     </aside>
   );
 }
